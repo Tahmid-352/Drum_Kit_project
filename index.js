8deb8bdf4de838474fe411756761f34496da772e
Md. Tahmid Khan <86355248+Tahmid-352@users.noreply.github.com>
2023-08-14 02:03:24 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+// Detecting button press
+var Number_of_drum = document.querySelectorAll(".drum").length;
+for(var i = 0; i < Number_of_drum; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+
+    function handleClick(){
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+        buttonAnnimation(buttonInnerHtml);
+    }
+}
+// Detecting keyboard press
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    buttonAnnimation(event.key);
+});
+
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+            var crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio('./sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio('./sounds/snare.mp3');
+            snare.play();
+            break; 
+        case "d":
+            var tom1 = new Audio('./sounds/tom-1.mp3');
+            tom1.play();
+            break; 
+        case "j":
+            var tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play();
+            break; 
+        case "k":
+            var tom3 = new Audio('./sounds/tom-3.mp3');
+            tom3.play();
+            break;  
+        case "l":
+            var tom4 = new Audio('./sounds/tom-4.mp3');
+            tom4.play();
+            break;               
+
+    
+        default:
+            console.log(buttonInnerHtml);
+            break;
+    }
+}
+
+function buttonAnnimation(currenyKey){
+    var activeButton = document.querySelector("." + currenyKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 200);
+}
